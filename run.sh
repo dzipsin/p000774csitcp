afd534d96f4b65fdff61d882bdf53434a78e8e5b
Dylan Zipsin <dylan.zipsin@gmail.com>
2026-06-11 14:54:06 +1000

    Added docker compose for dvwa and a setup README.md, and integrated it into run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
 sudo systemctl start suricata
 systemctl is-active --quiet suricata && echo "[+] Suricata: running" || echo "[!] Suricata: NOT running"
 
-cd ~/docker/dvwa
-docker compose up -d
+(cd dvwa && docker compose up -d)
 
-cd ~/p000774csitcp
 chmod +x ./setup_linux.sh
 ./setup_linux.sh >/dev/null
 source .venv/bin/activate && python3 src/app.py
